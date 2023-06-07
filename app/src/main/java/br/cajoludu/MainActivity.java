3142b2a05886cb88a8697d610d25d9f1d3be2926
João Vitor Mendes Pinto dos Santos <joao.v.m.p.santos@ieee.org>
2023-06-06 22:00:23 -0300

    feat: Firebase connection and Library and BookView class

--- app/src/main/java/br/cajoludu/MainActivity.java
+++ app/src/main/java/br/cajoludu/MainActivity.java
@@ -5,17 +5,26 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Context;
 import android.os.Bundle;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class MainActivity extends AppCompatActivity {
+
+    private DatabaseReference reference = FirebaseDatabase.getInstance().getReference();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        reference.child("Permissoes").setValue("all");
+        reference.child("Teste").setValue("VoMEMA");
+
         Context context = MainActivity.this; // Replace YourActivity with your actual activity or context
         String downloadUrl = "https://wiki.sj.ifsc.edu.br/images/7/7f/FTP-RES2014-1.pdf";
         String fileName = "FTP-RES2014-1.pdf";
 
         FileDownloader fileDownloader = new FileDownloader(context, downloadUrl, fileName);
-        fileDownloader.startDownload();
+        //fileDownloader.startDownload();
     }
 }
