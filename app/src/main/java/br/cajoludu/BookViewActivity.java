c691bc8abb2697bf4c3ff83e8e8c63284dd453cc
João Vitor Mendes Pinto dos Santos <joao.v.m.p.santos@ieee.org>
2023-06-08 19:14:26 -0300

    refacotr: Add function to downloadFile in button donwloadButton in BookViewActivity

--- app/src/main/java/br/cajoludu/BookViewActivity.java
+++ app/src/main/java/br/cajoludu/BookViewActivity.java
@@ -2,13 +2,20 @@ package br.cajoludu;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import br.cajoludu.model.Book;
 
 public class BookViewActivity extends AppCompatActivity {
+    private Button downloadButton;
+    private Context context;
+    private String downloadUrl = "https://wiki.sj.ifsc.edu.br/images/7/7f/FTP-RES2014-1.pdf";
+    private String fileName = "FTP-RES2014-1.pdf";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,5 +32,16 @@ public class BookViewActivity extends AppCompatActivity {
 
         TextView sinopse = findViewById(R.id.textView9);
         sinopse.setText(sinopseLivro);
+
+        context = BookViewActivity.this;
+
+        downloadButton = findViewById(R.id.buttonDownload);
+        downloadButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FileDownloader fileDownloader = new FileDownloader(context, downloadUrl, fileName);
+                fileDownloader.startDownload();
+            }
+        });
     }
 }
