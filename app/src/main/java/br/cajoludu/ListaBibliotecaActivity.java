7ffb458ed69e3367d65704c5437b386baf72b0a4
João Vitor Mendes Pinto dos Santos <joao.v.m.p.santos@ieee.org>
2023-06-08 22:43:40 -0300

    feat: Add a for each interation with firebase livros data and create mutiple tableRows in ListaBibliotecaActivity.java

--- app/src/main/java/br/cajoludu/ListaBibliotecaActivity.java
+++ app/src/main/java/br/cajoludu/ListaBibliotecaActivity.java
@@ -25,10 +25,7 @@ import br.cajoludu.model.Book;
 public class ListaBibliotecaActivity extends AppCompatActivity {
     private DatabaseReference reference = FirebaseDatabase.getInstance().getReference();
     private TableLayout tableLayout;
-    private Book mlivro;
     DatabaseReference livros = reference.child("livros");
-    private String id = "001";
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,21 +34,64 @@ public class ListaBibliotecaActivity extends AppCompatActivity {
 
         tableLayout = findViewById(R.id.tableLayout); // Assuming you have a TableLayout with id "tableLayout"
 
-        livros.child(id).addListenerForSingleValueEvent(new ValueEventListener() {
+        livros.addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
                 if (snapshot.exists()) {
-                    // Recupera os dados do snapshot
-                    String nome = snapshot.child("nome").getValue(String.class);
-                    int curtidas = snapshot.child("curtidas").getValue(int.class);
-                    String sinopse = snapshot.child("sinopse").getValue(String.class);
-                    String url = snapshot.child("url").getValue(String.class);
+                    for (DataSnapshot livroSnapshot : snapshot.getChildren()) {
+                        // Recupera os dados do snapshot do livro
+                        String nome = livroSnapshot.child("nome").getValue(String.class);
+                        int curtidas = livroSnapshot.child("curtidas").getValue(int.class);
+                        String sinopse = livroSnapshot.child("sinopse").getValue(String.class);
+                        String url = livroSnapshot.child("url").getValue(String.class);
 
-                    // Cria o objeto Book
-                    mlivro = new Book(nome, curtidas, sinopse, url);
+                        // Cria um novo objeto Book para cada livro
+                        Book livro = new Book(nome, curtidas, sinopse, url);
 
-                    // Chama o método para criar a tabela
-                    criarTabela();
+                        // Cria uma nova TableRow
+                        TableRow tableRow = new TableRow(ListaBibliotecaActivity.this);
+
+                        // Cria um novo ImageButton
+                        ImageButton imageButton = new ImageButton(ListaBibliotecaActivity.this);
+                        // Define o recurso de imagem para o ImageButton
+                        imageButton.setImageResource(R.drawable.livro);
+                        imageButton.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+                                abrirBookView(livro);
+                            }
+                        });
+
+                        // Cria um novo TextView
+                        TextView textView = new TextView(ListaBibliotecaActivity.this);
+                        // Define o texto para o TextView
+                        textView.setText(livro.getNome());
+
+                        // Define os parâmetros de layout para a TableRow
+                        TableRow.LayoutParams rowParams = new TableRow.LayoutParams(
+                                ViewGroup.LayoutParams.MATCH_PARENT, // Width
+                                ViewGroup.LayoutParams.WRAP_CONTENT // Height
+                        );
+
+                        // Define os parâmetros de layout para o ImageButton
+                        TableRow.LayoutParams imageParams = new TableRow.LayoutParams(
+                                ViewGroup.LayoutParams.WRAP_CONTENT, // Width
+                                ViewGroup.LayoutParams.WRAP_CONTENT // Height
+                        );
+
+                        // Define os parâmetros de layout para o TextView
+                        TableRow.LayoutParams textParams = new TableRow.LayoutParams(
+                                ViewGroup.LayoutParams.WRAP_CONTENT, // Width
+                                ViewGroup.LayoutParams.WRAP_CONTENT // Height
+                        );
+
+                        // Adiciona o ImageButton e o TextView à TableRow com seus respectivos parâmetros de layout
+                        tableRow.addView(imageButton, imageParams);
+                        tableRow.addView(textView, textParams);
+
+                        // Adiciona a TableRow ao TableLayout
+                        tableLayout.addView(tableRow, rowParams);
+                    }
                 }
             }
 
@@ -62,60 +102,11 @@ public class ListaBibliotecaActivity extends AppCompatActivity {
         });
     }
 
-    private void criarTabela() {
-        // Create a new TableRow
-        TableRow tableRow = new TableRow(this);
-
-        // Create an ImageButton
-        ImageButton imageButton = new ImageButton(this);
-        // Set image resource for the ImageButton
-        imageButton.setImageResource(R.drawable.livro);
-        imageButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                abrirBookView();
-            }
-        });
-
-        // Create a TextView
-        TextView textView = new TextView(this);
-        // Set text for the TextView
-        textView.setText(mlivro.getNome());
-
-        // Create layout parameters for the TableRow
-        TableRow.LayoutParams rowParams = new TableRow.LayoutParams(
-                ViewGroup.LayoutParams.MATCH_PARENT, // Width
-                ViewGroup.LayoutParams.WRAP_CONTENT // Height
-        );
-
-        // Set layout parameters for the TableRow
-        tableRow.setLayoutParams(rowParams);
-
-        // Create layout parameters for the ImageButton
-        TableRow.LayoutParams imageParams = new TableRow.LayoutParams(
-                ViewGroup.LayoutParams.WRAP_CONTENT, // Width
-                ViewGroup.LayoutParams.WRAP_CONTENT // Height
-        );
-
-        // Create layout parameters for the TextView
-        TableRow.LayoutParams textParams = new TableRow.LayoutParams(
-                ViewGroup.LayoutParams.WRAP_CONTENT, // Width
-                ViewGroup.LayoutParams.WRAP_CONTENT // Height
-        );
-
-        // Add the ImageButton and TextView to the TableRow with their respective layout parameters
-        tableRow.addView(imageButton, imageParams);
-        tableRow.addView(textView, textParams);
-
-        // Add the TableRow to the TableLayout
-        tableLayout.addView(tableRow, rowParams);
-    }
-
-    public void abrirBookView() {
+    public void abrirBookView(Book livro) {
         Intent intent = new Intent(this, BookViewActivity.class);
-        intent.putExtra("livroTitulo", mlivro.getNome());
-        intent.putExtra("livroSinopse", mlivro.getSinopse());
-        intent.putExtra("livrourl", mlivro.getUrl());
+        intent.putExtra("livroTitulo", livro.getNome());
+        intent.putExtra("livroSinopse", livro.getSinopse());
+        intent.putExtra("livrourl", livro.getUrl());
         startActivity(intent);
     }
 }
